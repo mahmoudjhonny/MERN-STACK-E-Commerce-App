f1b04e0de60e49867a3a1877ac462dacc7209bcc
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2022-09-28 12:24:34 +0200

    Initialize frontEnd Project

---
+++ front-end/src/Components/Slider.jsx
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+import { ArrowLeft, ArrowRight } from "@mui/icons-material";
+import { useState } from "react";
+import { sliderItems } from "../data";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  opacity: 0.5;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slide * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+
+const ImageContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+
+const Img = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Desc = styled.p`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+const Slider = () => {
+  const [slide, setslide] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setslide(slide > 0 ? slide - 1 : 2);
+    } else {
+      setslide(slide < 2 ? slide + 1 : 0);
+    }
+  };
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeft />
+      </Arrow>
+      <Wrapper slide={slide}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg} key={item.id}>
+            <ImageContainer>
+              <Img src={item.img} />
+            </ImageContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOP NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRight />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
