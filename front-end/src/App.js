c5a69473ad538582e11a6164fe750aace0c56a7a
Mahmoud Mostafa <mahmoud309mostafa@gmail.com>
2022-10-19 18:50:13 +0200

    Add react-routr-dom package

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,9 +5,21 @@ import Login from "./Pages/Login";
 import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
 import Register from "./Pages/Register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Product />;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
